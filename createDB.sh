9eeeb2a34e612d49d0db0261eada5cfbea37c951
Omar Hamroush <omarhamroush6@gmail.com>
2022-12-12 21:43:38 +0200

    first two scriptes for database menu

--- createDB.sh
+++ createDB.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+echo "Creating DataBase...."
+PS3='Please enter your choice: '
+options=("List Current DataBases" "Create New DataBase" "Main Menu"  "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "List Current DataBases")
+            ls -f | grep/ ./ITIDataBases    #in this folder we will save the new databases
+
+            ;;
+        "Create New DataBase")
+            read -p "Enter New DataBase Name : " DBname
+	    if [ -e $DBname ]; then
+		    echo "DataBase already Exists"
+	    else
+		    mkdir ./ITIDataBases/$DBname
+		    cd ./ITIDataBases/$DBname
+		    echo "DataBase Successfully Created"
+		    pwd
+		    #then we should call the create table script
+	    fi
+
+	    ;;
+        "Main Menu")
+            #call the main menu script
+
+            ;;
+        "Exit")
+            break
+
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
