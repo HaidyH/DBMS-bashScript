b99280df5de1c8b83cae44b9c108a7b779f3b27a
Omar Hamroush <omarhamroush6@gmail.com>
2022-12-17 22:26:39 +0200

    drop table script not complete

---
+++ DropTable.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Dropping Table...."
+PS3='Please enter your choice: '
+options=("List Current Tabless" "Drop Table" "Table Main Menu"  "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "List Current Tables")
+            ls -f | grep/ #path of choosen database    
+
+            ;;
+        "Drop Table")
+            read -p "Enter Table Name : " Tname
+            if [ -e $Tname ]; then
+                    rm -r $Tname
+		    echo "Table Dropped Successfully"
+            else
+                    echo "Table Not Found"
+            fi
+
+            ;;
+        "Table Main Menu")
+            #call the table main menu script
+
+            ;;
+        "Exit")
+            break
+
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
